76c38c71f213d56768d2b71d072e00249da11260
codwerk <louisws@outlook.fr>
2021-12-18 18:05:34 +0100

    component header

---
+++ my-blog/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="text-center bg-emerald-600 text-white p-4 fixed w-full">
+    <div class="text-3xl md:text-5xl font-bold"> My blog</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
