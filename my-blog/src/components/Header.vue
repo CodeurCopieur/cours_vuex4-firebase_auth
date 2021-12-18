99138cc2c2d2b3aa0bc2159b7ae2186879ede79d
codwerk <louisws@outlook.fr>
2021-12-19 00:52:44 +0100

    new link : home

--- my-blog/src/components/Header.vue
+++ my-blog/src/components/Header.vue
@@ -1,19 +1,20 @@
 <template>
   <header class="text-center bg-sky-600 text-white p-4">
     <div class="container mx-auto flex flex-wrap px-5 flex-col md:flex-row items-center">
-      <router-link to='/' class="flex title-font font-medium items-center">
-        <h1 class="ml-3 text-xl">Vue Auth</h1>
+      <router-link to='/' class="flex title-font font-bold items-center">
+        <h1 class="ml-3 text-2xl">Vue Auth</h1>
       </router-link>
       <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <!-- logged in -->
         <div>
-          <span>loggin in as...</span>
-          <button>Logout</button>
+          <span class="mr-5">loggin in as...</span>
+          <button class="mr-5">Logout</button>
         </div>
         <!-- logged out -->
         <div>
-          <router-link to='/login' class="mr-5 hover:text-gray-900">Login</router-link>
-          <router-link to='/signup' class="mr-5 hover:text-gray-900">Signup</router-link>
+          <router-link to='/home' class="mr-5 font-medium hover:text-gray-900">Home</router-link>
+          <router-link to='/login' class="mr-5 font-medium  hover:text-gray-900">Login</router-link>
+          <router-link to='/signup' class="mr-5 font-medium  hover:text-gray-900">Signup</router-link>
         </div>
         
       </nav>
