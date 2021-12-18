3b540f1b40f54757ab6309a6925fc10aac59b3f4
codwerk <louisws@outlook.fr>
2021-12-18 23:03:46 +0100

    links (nav)

--- my-blog/src/components/Header.vue
+++ my-blog/src/components/Header.vue
@@ -1,6 +1,23 @@
 <template>
-  <header class="text-center bg-emerald-600 text-white p-4 fixed w-full">
-    <div class="text-3xl md:text-5xl font-bold"> My blog</div>
+  <header class="text-center bg-sky-600 text-white p-4">
+    <div class="container mx-auto flex flex-wrap px-5 flex-col md:flex-row items-center">
+      <router-link to='/' class="flex title-font font-medium items-center">
+        <h1 class="ml-3 text-xl">Vue Auth</h1>
+      </router-link>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <!-- logged in -->
+        <div>
+          <span>loggin in as...</span>
+          <button>Logout</button>
+        </div>
+        <!-- logged out -->
+        <div>
+          <router-link to='/login' class="mr-5 hover:text-gray-900">Login</router-link>
+          <router-link to='/signup' class="mr-5 hover:text-gray-900">Signup</router-link>
+        </div>
+        
+      </nav>
+    </div>
   </header>
 </template>
 
