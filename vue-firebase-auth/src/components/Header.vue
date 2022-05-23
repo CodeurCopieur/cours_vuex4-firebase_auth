fe1af8866bb4dbed9580cf9cbcbcad7412aaaaa9
Widdy LOUIS <louisws@outlook.fr>
2022-05-24 00:16:38 +0200

    Component Header

---
+++ vue-firebase-auth/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+  <header class="text-center bg-sky-600 text-white p-4">
+    <div class="container mx-auto flex flex-wrap px-5 flex-col md:flex-row items-center">
+      <router-link to='/' class="flex title-font font-bold items-center">
+        <h1 class="ml-3 text-2xl">Vue Auth</h1>
+      </router-link>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <router-link to='/feed' class="mr-5 font-medium hover:text-gray-900">Feed</router-link>
+        <router-link to='/register' class="mr-5 font-medium hover:text-gray-900">Register</router-link>
+        <router-link to='/sign-in' class="mr-5 font-medium hover:text-gray-900">Login</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
