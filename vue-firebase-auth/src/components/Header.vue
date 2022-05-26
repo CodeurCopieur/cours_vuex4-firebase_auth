914be916803042b6868b98f69bcfb699e3916200
Widdy LOUIS <louisws@outlook.fr>
2022-05-26 20:54:09 +0200

    end signout, redirection si non connecté

--- vue-firebase-auth/src/components/Header.vue
+++ vue-firebase-auth/src/components/Header.vue
@@ -1,5 +1,30 @@
 <script setup>
+  import { onMounted, ref } from 'vue';
+  import { getAuth, onAuthStateChanged, signOut } from '@firebase/auth';
+  import { useRouter } from 'vue-router'
 
+  const isLoggedIn = ref(false);
+  const router = useRouter(); // obtenir une référence à notre routeur vue
+
+  let auth;
+  onMounted( () => {
+    auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        isLoggedIn.value = true;     
+      } else {
+        isLoggedIn.value = false;
+      }
+    })
+  });
+
+  // tout ce que nous avons à faire est d'appeler signout(auth) depuis firebase/auth
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      router.push("/")
+    })
+  };
 </script>
 <template>
   <header class="text-center bg-sky-600 text-white p-4">
@@ -11,6 +36,7 @@
         <router-link to='/feed' class="mr-5 font-medium hover:text-gray-900">Feed</router-link>
         <router-link to='/register' class="mr-5 font-medium hover:text-gray-900">Register</router-link>
         <router-link to='/sign-in' class="mr-5 font-medium hover:text-gray-900">Login</router-link>
+        <button class="mr-5" @click="handleSignOut" v-if="isLoggedIn">Logout</button>
       </nav>
     </div>
   </header>
--- vue-firebase-auth/src/router/index.js
+++ vue-firebase-auth/src/router/index.js
@@ -1,15 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-  {path: '/', component: () => import('../views/Home.vue')},
-  {path: '/register', component: () => import('../views/Register.vue')},
-  {path: '/sign-in', component: () => import('../views/SignIn.vue')},
-  {path: '/feed', component: () => import('../views/Feed.vue')},
-]
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes: [
+    {path: '/', component: () => import('../views/Home.vue')},
+    {path: '/register', component: () => import('../views/Register.vue')},
+    {path: '/sign-in', component: () => import('../views/SignIn.vue')},
+    {
+      path: '/feed', 
+      component: () => import('../views/Feed.vue'),
+      meta: {
+        requiresAuth: true,
+      }
+    },
+  ]  
+});
+
+// nous avons donc une authentification de base,  protéger /feed ?
+
+const getCurrentUser = ()=> {
+  return new Promise( (resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+      reject
+    );
+  });
+};
+
+// utilise vue-router navigation guards qui s'exécute avant chaque itinéraire
+
+router.beforeEach(async(to, from, next) => {
+  if(to.matched.some((record) => record.meta.requiresAuth)) {
+    if(await getCurrentUser()) {
+      next();
+    } else {
+      next("/");
+      alert("vous n'y avez pas accès !");
+    }
+  } else {
+    next();
+  }
+});
 
 export default router;
